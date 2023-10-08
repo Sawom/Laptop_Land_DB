acd200efd0f0ce6f8accf83d533b3a1a6bb559fb
Sawom <asawom250@gmail.com>
2023-10-08 17:34:21 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bsdjaxv.mongodb.net/?retryWrites=true&w=majority` ;
+const client = new MongoClient(uri, { serverApi: {version: ServerApiVersion.v1, strict: true, deprecationErrors: true, }});
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('laptop land is running');
+})
+
+app.listen(port, ()=> {
+    console.log(`laptop land server running at ${port}` );
+}) 
